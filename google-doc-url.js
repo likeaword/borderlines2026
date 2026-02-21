7bb5d4cf27f04fb197a8a55055eeba34693fa4dc
Ben Proctor <ben@benproctor.co.uk>
2026-02-21 17:06:28 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wtzC-otnaHb3wmb-13TKO6lKjM3862jTOqPm_VI02S8/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDHSMW5yk6yUYlYN60GhxLgFUhvKHTNUdM';
